de19aaa11125b0af912829c323d53c9f7a0cc5eb
pedrofelipe <pf.souza15@gmail.com>
2021-08-25 13:53:46 -0300

    improving category list component template

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  deleteCategory() {
+  }
+
+  editCategory(id){
+     this.router.navigate(['categories/' + id, 'edit'])
+  }
 }
